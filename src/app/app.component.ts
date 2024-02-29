ba7ea412b47e0ad3e028313559350d92b9a5aaef
japago25andes <j.pardog@uniandes.edu.co>
2024-02-29 13:50:54 -0500

    Contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Hola mundo';
+  public counter: number = 10;
+
+  increaseBy(option: number):void {
+
+    this.counter += option;
+
+  }
+
+  resetCounter():void {
+
+    this.counter = 10;
+
+  }
+
+
+
+
+
+
 }
